27115732cbdfca7020efb7aa9dd7f9fa6bab3b85
shr1k <110760070+schr1k@users.noreply.github.com>
2023-05-02 18:57:46 +0300

    add config.py

---
+++ config.py
@@ -0,0 +1,59 @@
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from enum import Enum
+# тут достаточно pip install aiogram
+
+TOKEN = 'Токен от BotFather'
+ADMINS = ['id админов через запятую']  # пока что в боте это нигде не используется ):
+DB = 'db.db'  # имя файла базы данных
+
+RETURN_URL = 'https://t.me/anonchik_chat_bot'  # ссылка для возврата после оплаты
+
+# payok
+API_ID = 'int'
+API_KEY = 'str'
+SHOP_ID = 'int'
+SECRET_KEY = 'str'
+
+
+# vip_day = 'ссылка на оплату (payok)'
+# vip_week = 'ссылка на оплату (payok)'
+# vip_month = 'ссылка на оплату (payok)'
+
+
+class RegState(StatesGroup):
+	name = State()
+	sex = State()
+	age = State()
+	country = State()
+	city = State()
+	op_sex = State()
+
+
+class SetName(Enum):
+	nothing = 'nothing_name'
+	waiting = 'waiting_name'
+
+
+class SetAge(Enum):
+	nothing = 'nothing_age'
+	waiting = 'waiting_age'
+
+
+class SetSex(Enum):
+	nothing = 'nothing_sex'
+	waiting = 'waiting_sex'
+
+
+class SetCountry(Enum):
+	nothing = 'nothing_country'
+	waiting = 'waiting_country'
+
+
+class SetCity(Enum):
+	nothing = 'nothing_city'
+	waiting = 'waiting_waiting'
+
+
+class SetOpSex(Enum):
+	nothing = 'nothing_op_sex'
+	waiting = 'waiting_op_sex'
