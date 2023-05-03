b88d70fdc83fea9e6442a976c2fd2d81793326ac
shr1k <110760070+schr1k@users.noreply.github.com>
2023-05-03 15:46:11 +0300

    Create create_db.py

---
+++ create_db.py
@@ -0,0 +1,10 @@
+import sqlite3
+
+try:
+	connection = sqlite3.connect("db.db")
+	connection.execute(
+		"create table users (id integer primary key autoincrement, state varchar(20), name varchar(20), age varchar(3), sex varchar(10), country varchar(20), city varchar(20), op_sex varchar(10), tg_username varchar(20), tg_id varchar(25), tg_first_name varchar(20), tg_second_name varchar(20), connect_with varchar(20), last_connect varchar(20), chats int default 0, messages int default 0, likes int default 0, dislikes int default 0, vip_ends varchar(30), refs int default 0, points int default 0, notifications int default 1)")
+	connection.execute("create table all_messages (sender varchar(20), message varchar(30))")
+	connection.execute("create table queue (tg_id varchar(15), sex varchar(6), op_sex varchar(6))")
+except Exception as e:
+	print(e)
