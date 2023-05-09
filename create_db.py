b789c37d7de3852d2e0d16bbb25ff7de04eb4bcd
shr1k <110760070+schr1k@users.noreply.github.com>
2023-05-09 21:23:36 +0300

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -1,7 +1,8 @@
 import sqlite3
+import config
 
 try:
-	connection = sqlite3.connect("db.db")
+	connection = sqlite3.connect(config.DB)
 	connection.execute(
 		"create table users (`id` integer primary key autoincrement, `state` varchar(20), `name` varchar(20), `age` varchar(3), `sex` varchar(10), `country` varchar(20), `city` varchar(20), `op_sex` varchar(10), `tg_id` varchar(25), `connect_with` varchar(20), `last_connect` varchar(20), `chats` int default 0, `messages` int default 0, `likes` int default 0, `dislikes` int default 0, `vip_ends` varchar(30), `refs` int default 0, `points` int default 0, `notifications` int default 1, `order_id` int default 0)")
 	connection.execute("create table all_messages (`sender` varchar(20), `message` varchar(30))")
